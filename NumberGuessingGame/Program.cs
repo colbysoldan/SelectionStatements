9ad1b4575b6ed73fdba2c11e38f9018b12222aa0
Colby Soldan <colbylsoldan@gmail.com>
2022-06-02 14:40:46 -0500

    added dowhile loop

--- NumberGuessingGame/Program.cs
+++ NumberGuessingGame/Program.cs
@@ -10,25 +10,28 @@ namespace NumberGuessingGame
 
             var r = new Random();
             var favNumber = r.Next(1, 1000);
-
-            Console.WriteLine("Try to guess my favorite number!" +
-                "\nPick a number between 1 and 1000.");
-            var userInput = int.Parse(Console.ReadLine());
-
-            if (userInput > favNumber)
+            int userInput;
+            do
             {
-                Console.WriteLine("Too high!");
-                int.Parse(Console.ReadLine());
-            }
+                Console.WriteLine("Try to guess my favorite number!" +
+                    "\nPick a number between 1 and 1000.");
+                userInput = int.Parse(Console.ReadLine());
 
-            else if (userInput < favNumber)
-            {
-                Console.WriteLine("Too low!");
-            }
-            else
-            {
-                Console.WriteLine("You guessed it!");
-            }
+                if (userInput > favNumber)
+                {
+                    Console.WriteLine("Too high!");
+                    int.Parse(Console.ReadLine());
+                }
+
+                else if (userInput < favNumber)
+                {
+                    Console.WriteLine("Too low!");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                }
+            } while (favNumber != userInput);
 
             Console.ReadLine();
 
